7f961fbd820e82b826c64c3de5fb23ef9f26d064
Zachary Hickson <zhickson@dunktree.com>
2019-08-29 18:19:51 +1000

    scaffold tests with wp-cli

---
+++ tests/test-cfi.php
@@ -0,0 +1,20 @@
+<?php 
+/**
+ * Class Contextual_Featured_ImagesTest
+ *
+ * @package Contextual_Featured_Images
+ */
+
+/**
+ * Primary test case.
+ */
+
+class Contextual_Featured_ImagesTest extends WP_UnitTestCase {
+
+    public function setUp() {
+        parent::setUp();
+
+        $this->class_instance = new Contextual_Featured_Images();
+    }
+
+}
